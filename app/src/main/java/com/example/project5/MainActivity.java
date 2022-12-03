150a01a0eb901c4e7ccc85dda2d3b0fd4e84ed94
Aaron Newland <51378352+aaronnewland@users.noreply.github.com>
2022-12-03 17:45:13 -0500

    Added ChicagoActivity stub

--- app/src/main/java/com/example/project5/MainActivity.java
+++ app/src/main/java/com/example/project5/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.project5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton chicagoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        chicagoButton = (ImageButton) findViewById(R.id.chicagoButton);
+        System.out.println("here");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, ChicagoActivity.class);
+        chicagoButton.setOnClickListener(view -> startActivity(intent));
+    }
+
+
 }
